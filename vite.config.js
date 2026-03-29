bd8853c0ff397e888cf5137dfd486029660f4b0f
pentaoo <alekseypnt@gmail.com>
2026-03-29 23:34:20 +0300

    chore: remove vite tooling from github pages branch

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-const FALLBACK_REPO_NAME = "nosok-the-game";
-const repositoryName =
-  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? FALLBACK_REPO_NAME;
-
-export default defineConfig({
-  base: `/${repositoryName}/`,
-});
